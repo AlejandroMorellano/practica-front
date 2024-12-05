081e6e12a3e98732a9d5d8a8bbc5a3e1d4496aed
AlejandroMorellano <20213tn026@utez.edu.mx>
2024-12-04 18:30:37 -0600

    dios

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>Home</h1>
-      <p>Bienvenido a la página de inicio. Lo porque</p>
+      <p>Bienvenido a la página de inicio. ya quedo</p>
     </div>
   </template>
   
